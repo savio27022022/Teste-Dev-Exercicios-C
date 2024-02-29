9b4a39d2ddc88e90eaec451faaf04f665ef9eb81
Sávio <115809910+savio27022022@users.noreply.github.com>
2024-02-29 15:01:53 -0300

    Create Ordem Decrescente.c

---
+++ Ordem Decrescente.c
@@ -0,0 +1,45 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<locale.h>
+
+ void()cabecalho(){
+ printf("Ordem Decrescente");
+system("cls||clear");
+}
+int main(){
+setlocale(LC_ALL," ");
+int j;
+cabecalho();
+for(j=10;j>=1;j--){
+printf("%d\n",j);
+}
+return 0;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+

640d98fc00e23d860a22765eae04cde61312dcd0
Sávio <115809910+savio27022022@users.noreply.github.com>
2024-03-01 07:27:43 -0300

    Create listanumerospares100120for.c

---
+++ listanumerospares100120for.c
@@ -0,0 +1,41 @@
+#include<stdio.h>
+#include<locale.h>
+void cabecalho(){
+printf("\n====Listagem de Pares=====\n");
+system("cls||clear");
+fflush(stdin);
+}
+int main(){
+setlocale(LC_ALL," portuguese"); 
+int i;
+cabecalho();
+printf("Lista de números Pares:\n");
+for(i=100;i<=120;i++){
+}
+  if(i=%2==0){
+    printf("%d\n",i); 
+     }
+}
+return 0;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+

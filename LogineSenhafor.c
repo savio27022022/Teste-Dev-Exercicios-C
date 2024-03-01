812e6658fbbbce5744a5c03d462365c0e07ae238
Sávio <115809910+savio27022022@users.noreply.github.com>
2024-03-01 07:18:41 -0300

    Update and rename UsuarioeSenhafor.c to LogineSenhafor.c

--- LogineSenhafor.c
+++ LogineSenhafor.c
@@ -2,31 +2,31 @@
 #include<stdio.h>
 #include<locale.h>
 void cabecalho(){
-printf("\n==== SENAI=====\n");
+printf("\n====Login e Senha=====\n");
 system("cls||clear");
 fflush(stdin);
 }
 int main(){
 setlocale(LC_ALL,"portuguese");
 //Declaração das variáveis.
-char emailCadastrado[200]="saviosantos98266@gmail.com";
-char senhaCadastrada[200]="1234567890";
-char emailUsuario[200];
-char senhaUsuario[200];
+char loginCadastrado[200]="savio01";
+char senhaCadastrada[200]="1234";
+char login[200];
+char senha[200];
 
 
 //Solicite os dados ao Usúario.
 cabecalho();
-printf("\nDigite o email do Usuário:");
-scanf("%s",&emailUsuario);
+printf("\nDigite o login do Usuário:");
+scanf("%s",&login);
 
 cabecalho();
 printf("\nDigite a senha do Usuário:");
-scanf("%s",&senhaUsuario);
+scanf("%s",&senha);
 //Definindo se as informações do usuário são válidas ou não e dando os resultaodos.
 cabecalho();
-if(strcmp(emailCadastrado"saviosantos98266@hotmail.com",emailUsuario" ")==0 &&
- strcmp(senhaCadastrada"1234567890",senhaUsuario" ")==0){
+ if(strcmp(login,loginCadastrado)==0 &&
+ strcmp(senha,senhaCadastrada)==0){
 cabecalho();
 printf("\nSeja Bem-Vindo(a):");
 }else{

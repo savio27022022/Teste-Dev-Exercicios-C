f22ef4f3f0537b97a93639a5bffee402f5101bee
Sávio <115809910+savio27022022@users.noreply.github.com>
2024-03-01 07:33:49 -0300

    Create quantidadenumerosparesimparesfor.c

---
+++ quantidadenumerosparesimparesfor.c
@@ -0,0 +1,54 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<locale.h>
+
+void()cabecalho(){
+printf("\n===05 números pares e ímpares  quantidade===");
+system("cls||clear");
+fflush(stdin);
+
+int main(){
+setlocale(LC_ALL," portuguese");
+
+int numero;
+int par=0;
+int impar=0;
+
+//Loop para solicitar 
+cabecalho();
+for(int i=0; i<5;i++){
+printf("\nDigite o %dº número:",i+1);
+scanf("%d",&numero);
+
+if(numero%2===0){
+   par++
+}else{
+  lmpar++
+
+if(numero%2==0){
+   par++
+}else{
+   Impar++
+
+if(numero%2==0){
+   par++
+}else{
+   Impar++
+
+if(numero%2==0){
+    par ++
+}else{
+   Impar++
+
+if(numero%2==0){
+    par++
+}else{
+   Impar++
+
+}
+
+printf("Os números par são:%d e ímpar são:%d",par,impar);
+
+return 0;
+
+}

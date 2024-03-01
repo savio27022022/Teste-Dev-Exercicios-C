d360961be32e3693de7a34c46ee1130f03ac9094
Sávio <115809910+savio27022022@users.noreply.github.com>
2024-03-01 07:30:28 -0300

    Create Listagemnumerosimpares1e20for.c

---
+++ Listagemnumerosimpares1e20for.c
@@ -0,0 +1,21 @@
+#include<stdlib.h>
+#include<stdio.h>
+#include<locale.h>
+void cabecalho(){
+printf("\n==== Listagem de Ímpares.===\n");
+system("cls||clear");
+fflush(stdin);
+}
+int main(){
+setlocale(LC_ALL," "); 
+int i;
+cabecalho();
+printf("Lista de números Ímpares:\n");
+for(i=1;i<=20;i++){
+}
+  if(i=%2!=0){
+    printf("%d\n",i); 
+     }
+}
+return 0;
+}
